58bfcf222afaa43470a404ea6f58980c49fe6e44
RGH3 <Hubbard3@live.missouristate.edu>
2018-01-26 13:02:28 -0600

    start 1/26/18 notes

---
+++ Lecture2018-26-01.py
@@ -0,0 +1,8 @@
+from csc131 import Dictionaries
+
+def main():
+    print("Main")
+
+
+if __name__ == '__main__':
+    main()
---
+++ csc131/Dictionaries.py
@@ -0,0 +1,10 @@
+#
+#1/26/18
+#
+
+def get_personal_data() ->dict:
+    """
+    :return: Returns a dictionary of personal information.
+    """
+    personal_data = {"name": "Jim", "role": "teacher"}
+    return personal_data
