7544c066015ff13b02e50a9a53b05cc2bfbae9e2
RGH3 <Hubbard3@live.missouristate.edu>
2018-01-29 13:10:14 -0600

    1/29/18 notes

--- csc131/Dictionaries.py
+++ csc131/Dictionaries.py
@@ -3,6 +3,8 @@
 '''edited 1/29/18'''
 #
 
+import string
+
 def get_personal_data() ->dict:
     """
     :return: Returns a dictionary of personal information.
@@ -17,7 +19,17 @@ def main() -> int:
     print(initialized_dict)
     simple_init_dict = dict(name = 'Jim', a_role = 'teacher')
     print(simple_init_dict)
+    simple_init_dict['a_role'] = 'joker'
+    print(simple_init_dict)
+    my_comprehension = {x: x**2 for x in range(5)}
+    print(my_comprehension)
+
+    #s = "a little lamb, it's fleece".split()
+    s = "little,".translate({ord(i): None for i in string.punctuation})
+    print(s)
+
+
     return 0
 
-if __name__ == '__main__':
+if __name__ =='__main__':
     main()
