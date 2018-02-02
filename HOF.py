5c498acf8dc80d1fa95f6b31981cd2bd21fc0dec
RGH3 <Hubbard3@live.missouristate.edu>
2018-02-02 12:58:43 -0600

    2/2/18 notes

--- HOF.py
+++ HOF.py
@@ -1,3 +1,5 @@
+import math
+
 def demo_function_temp_var(x: int) -> None:
     f = abs
     print("absolute value f(x) = ", f(x))
@@ -6,8 +8,9 @@ def demo_function_parameter(f, x:int) ->int:
     return f(x)
 
 def main() -> int:
-    demo_function_temp_var(-4)
-    y = demo_function_parameter(abs, -4)
+    y = demo_function_temp_var(-4)
+    print("absolute value f(x) = ", y)
+    y = demo_function_parameter(math.sqrt, 44)
     print("absolute value f(x) = ", y)
     return 0
 
